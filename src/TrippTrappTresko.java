549b91bb75e04f5812447f7c28ce556ee722f820
Thomasly89 <56018095+Thomasly89@users.noreply.github.com>
2020-12-08 20:41:00 +0100

    cpu player
    Added a player and a cpu player with random

--- src/TrippTrappTresko.java
+++ src/TrippTrappTresko.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TrippTrappTresko {
@@ -12,39 +13,17 @@ public class TrippTrappTresko {
         Scanner scan = new Scanner(System.in);
         System.out.println("Skriv inn din valgte posisojon (1-9):");
 
-        int pos = scan.nextInt();
+        int spillerPos = scan.nextInt();
 
-        System.out.println(pos);
+        System.out.println(spillerPos);
 
-        switch(pos){
-            case 1:
-                spillBrett[0][0] = 'X';
-                break;
-            case 2:
-                spillBrett[0][2] = 'X';
-                break;
-            case 3:
-                spillBrett[0][4] = 'X';
-                break;
-            case 4:
-                spillBrett[2][0] = 'X';
-                break;
-            case 5:
-                spillBrett[2][2] = 'X';
-                break;
-            case 6:
-                spillBrett[2][4] = 'X';
-                break;
-            case 7:
-                spillBrett[4][0] = 'X';
-                break;
-            case 8:
-                spillBrett[4][2] = 'X';
-                break;
-            case 9:
-                spillBrett[4][4] = 'X';
-                break;
-        }
+        plasserebrikke(spillBrett, spillerPos, "spiller");
+
+        Random rand = new Random();
+        int cpuPos = rand.nextInt(9) + 1;
+        plasserebrikke(spillBrett, cpuPos, "cpu");
+
+
         printSpillBrett(spillBrett);
     }
     public static void printSpillBrett(char [][] spillBrett){
@@ -53,6 +32,46 @@ public class TrippTrappTresko {
                 System.out.print(r);
             }
             System.out.println();
+        }
+    }
+    public static void plasserebrikke(char[][] spillBrett, int pos, String bruker){
+        char symbol = ' ';
+        if(bruker.equals("spiller")){
+            symbol = 'X';
+        }
+        else if(bruker.equals("cpu")){
+            symbol = 'O';
+        }
+        switch(pos){
+            case 1:
+                spillBrett[0][0] = symbol;
+                break;
+            case 2:
+                spillBrett[0][2] = symbol;
+                break;
+            case 3:
+                spillBrett[0][4] = symbol;
+                break;
+            case 4:
+                spillBrett[2][0] = symbol;
+                break;
+            case 5:
+                spillBrett[2][2] = symbol;
+                break;
+            case 6:
+                spillBrett[2][4] = symbol;
+                break;
+            case 7:
+                spillBrett[4][0] = symbol;
+                break;
+            case 8:
+                spillBrett[4][2] = symbol;
+                break;
+            case 9:
+                spillBrett[4][4] = symbol;
+                break;
+            default:
+                break;
         }
     }
 }
